b0f25e0353a2d40a8ec95502e355720fd948f049
CesarEspitia <livecesar@hotmail.com>
2020-01-31 19:15:08 -0500

    Student: Model, Controller and CreateStudent View

---
+++ GradesManager/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GradesManager.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateStudent()
+        {
+            ViewBag.Message = "Student Create Module";
+
+            return View();
+        }
+    }
+}
---
+++ GradesManager/Models/StudentModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GradesManager.Models
+{
+    public class StudentModel
+    {
+        public int id { get; set; }
+
+        // The restrictions placed here (Required...) are processed in the client, by JQuery
+
+        [Required(ErrorMessage = "You need to fill this field.")]
+        [Display(Name ="Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name ="Middle Name")]
+        public string SecondName { get; set; }
+
+        [Required(ErrorMessage ="You need to fill this field.")]
+        [Display(Name = "First Last Name")]
+        public string FirstLastName { get; set; }
+
+        [Display(Name ="Second Last Name")]
+        public string SecondLastName { get; set; }
+
+        [Required(ErrorMessage = "You need to fill this field.")]
+        [Range(100000, 9999999999, ErrorMessage = "You need to enter a valid Student DNI")]
+        public string DNI { get; set; }
+    }
+}
